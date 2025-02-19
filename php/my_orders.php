fd234da1febaf6abbbc3e483ec799520abae9377
Amro Hesham <mr487680@gmail.com>
2025-02-19 15:11:20 +0200

    adding Action Buttons

--- php/my_orders.php
+++ php/my_orders.php
@@ -30,7 +30,7 @@ require_once('my_orders(functions).php');
             </div>
             <div class="dropdown">
                 <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                <span class="ms-2"><?php echo isset($users['name']) ? $users['name'] : 'User'; ?></span>
+                <span class="ms-2"><?php echo isset($users['user_id']) ? $users['name'] : 'User'; ?></span>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item" href="logout.php">Logout</a></li>
@@ -58,16 +58,38 @@ require_once('my_orders(functions).php');
                             if (!empty($orders)) {
                                 foreach ($orders as $order) {
                                     $orderId = $order['order_id'];
+                                    $status = $order['status'];
                                     $totalPrice = calculateTotalPrice($db, $orderId);
+
                                     echo "<tr>";
                                     echo "<td>" . date("Y/m/d h:i A", strtotime($order['date'])) . "</td>";
-                                    echo "<td><span class='status-badge " . $order['status'] . "'>" . htmlspecialchars($order['status']) . "</span></td>";
+                                    echo "<td><span class='status-badge " . $status . "'>" . htmlspecialchars($status) . "</span></td>";
                                     echo "<td>$" . $totalPrice . "</td>";
-                                    echo "<td></td>";
+                                    echo "<td>";
+
+                                    // Edit button
+                                    echo "<a href='edit_order.php?order_id=$orderId' class='btn btn-primary btn-sm me-2'>
+                                            <i class='fa fa-edit'></i> Edit
+                                          </a>";
+
+                                    // Delete button with confirmation
+                                    echo "<a href='delete_order.php?order_id=$orderId' class='btn btn-danger btn-sm me-2'
+                                            onclick='return confirm(\"Are you sure you want to delete this order?\");'>
+                                            <i class='fa fa-trash'></i> Delete
+                                          </a>";
+
+                                    // Cancel button (only for Pending & Processing orders)
+                                    if ($status === "Pending" || $status === "Processing") {
+                                        echo "<a href='cancel_order.php?order_id=$orderId' class='btn btn-warning btn-sm'>
+                                                <i class='fa fa-times'></i> Cancel
+                                              </a>";
+                                    }
+
+                                    echo "</td>";
                                     echo "</tr>";
                                 }
                             } else {
-                                echo "<tr><td colspan='3' class='text-center py-4'>No orders found.</td></tr>";
+                                echo "<tr><td colspan='4' class='text-center py-4'>No orders found.</td></tr>";
                             }
                             ?>
                         </tbody>
